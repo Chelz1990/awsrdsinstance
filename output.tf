72cc8d2f63af79276c8f6333689965d52067eacf
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-05 15:37:02 -0500

    edited output

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "db_instance_endpoint" {
-  value       = aws_db_instance.mysql-db-server.endpoint
+  value       = aws_db_instance.dbserver.endpoint
+#  depends_on = aws_db_instance.dbserver
 }
