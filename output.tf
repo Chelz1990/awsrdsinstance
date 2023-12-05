3a48337c05376edfd0bd18b82ee054911f2fa54a
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-05 15:44:22 -0500

    edited output

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "db_instance_endpoint" {
-  value       = aws_db_instance.dbserver.endpoint
+  value       = aws_db_instance.ttplaydbserver.endpoint
   depends_on = [
     aws_db_instance.ttplaydbserver
   ]
