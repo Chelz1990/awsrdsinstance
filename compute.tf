74182332834d39bef12846f3b721dcb3e8da6fca
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-05 15:30:06 -0500

    edited network to include igw and subnet

--- compute.tf
+++ compute.tf
@@ -1,4 +1,10 @@
-resource "aws_instance" "tt-pbface" {
+resource "aws_instance" "tt-vm" {
   ami           = "ami-0fa1ca9559f1892ec"
   instance_type = "t3.micro"
+  user_data = <<HEREDOC
+  
+  #!/bin/bash
+  echo "hello world"
+
+  HEREDOC
 }
--- network.tf
+++ network.tf
@@ -1,7 +1,32 @@
+# create VPC
 resource "aws_vpc" "tt-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# create public subnet
+resource "aws_subnet" "tt-vm-sub" {
+  vpc_id     = aws_vpc.tt-vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+# create route table
+resource "aws_route_table" "tt-route" {
+  vpc_id = aws_vpc.tt-vpc.id
+}
+
+# create internet gateway
+resource "aws_internet_gateway" "tt-igw" {
+  vpc_id = aws_vpc.tt-vpc.id
+}
+
+# associate igw with route table
+resource "aws_route" "tt-public-igw" {
+  route_table_id         = aws_route_table.tt-route.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.tt-igw.id
+}
+
+# create security group
 resource "aws_security_group" "tt-sg" {
   name        = "3tier-security-group"
   vpc_id      = aws_vpc.tt-vpc.id
