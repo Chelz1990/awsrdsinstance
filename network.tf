1e2ca5500e52797d868c3c1c54a975287d0c78f0
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-05 15:11:53 -0500

    edited network

--- network.tf
+++ network.tf
@@ -5,22 +5,23 @@ resource "aws_vpc" "tt-vpc" {
 resource "aws_security_group" "tt-sg" {
   name        = "3tier-security-group"
   vpc_id      = aws_vpc.tt-vpc.id
-
+  
   dynamic "ingress" {
-
-    for_each = var.ingress_ports
+    for_each = [80,443,3306]
+    iterator = port
+    
     content {
-        from_port = ingress.value
-        to_port = ingress.value
-        protocol = tcp
-        cidr_blocks = ["0.0.0.0/0"]
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
-}
 
-data "aws_internet_gateway" "tt-gw" {
-  filter {
-    name   = "3tier-igw"
-    values = aws_vpc.tt-vpc.id
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
