ccb4a846d25231abaab0de3a484e8600e956c5ae
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-05 15:42:52 -0500

    added interpolation

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "db_instance_endpoint" {
   value       = aws_db_instance.dbserver.endpoint
-  depends_on = aws_db_instance.ttplaydbserver
+  depends_on = "${aws_db_instance.ttplaydbserver}"
 }
