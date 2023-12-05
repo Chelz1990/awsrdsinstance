4a97483debe00e84a696c5c5544c4633fdd51d4f
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-05 15:38:35 -0500

    edited db

--- database.tf
+++ database.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "dbserver" {
   engine_version = "5.7"
   allocated_storage    =  20
   instance_class = "db.t2.micro"
-  username       = "${random_string.login.result}"
+  username       = "admin"
   password       = "${random_string.password.result}"
   vpc_security_group_ids = ["${aws_security_group.tt-sg.id}"]
   skip_final_snapshot  = true
